33bbf7a040802cd7c9d0e27bad99a24a11cdae3f
Berkus Decker <berkus+github@metta.systems>
2022-05-03 22:59:42 +0300

    refactor!: Rename fast_alloc to freelist_alloc
    Add crate exports.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 #![cfg_attr(not(test), no_std)]
 
 pub mod buddy_alloc;
-pub mod fast_alloc;
+pub mod freelist_alloc;
 pub mod non_threadsafe_alloc;
 #[cfg(test)]
 mod tests;
 
-pub use crate::buddy_alloc::BuddyAllocParam;
-pub use crate::fast_alloc::FastAllocParam;
-pub use crate::non_threadsafe_alloc::NonThreadsafeAlloc;
+pub use crate::{
+    buddy_alloc::{BuddyAlloc, BuddyAllocParam},
+    freelist_alloc::{FreelistAlloc, FreelistAllocParam},
+    non_threadsafe_alloc::NonThreadsafeAlloc,
+};
